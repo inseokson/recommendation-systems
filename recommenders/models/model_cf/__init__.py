d1eb6a2e7f4facc25ff6a5b4348babc024ba18ce
InseokSon <inseokson92@gmail.com>
2022-05-19 23:24:35 +0900

    Feat: Matrix Factorization

---
+++ recommenders/models/model_cf/__init__.py
@@ -0,0 +1,5 @@
+from ._mf import MF
+
+__all__ = [
+    "MF",
+]
---
+++ recommenders/models/model_cf/_mf.py
@@ -0,0 +1,92 @@
+from typing import Callable, TypeVar, Union
+
+import pandas as pd
+import torch
+
+MFType = TypeVar("MFType", bound="MF")
+
+
+class MF(object):
+    def __init__(
+        self,
+        df: pd.DataFrame,
+        latent_dim: int,
+    ):
+        self.df = df
+        self.latent_dim = latent_dim
+
+        self.users, self.items = self.df.index, self.df.columns
+        self.loss = []
+
+    def _init_params(
+        self,
+    ):
+        self.P = torch.randn(len(self.users), self.latent_dim, requires_grad=True)
+        self.Q = torch.randn(self.latent_dim, len(self.items), requires_grad=True)
+
+        self.bias = self.df.mean().mean()
+        self.user_bias = torch.zeros(len(self.users), 1, requires_grad=True)
+        self.item_bias = torch.zeros(len(self.items), 1, requires_grad=True)
+
+        self.trainable_params = [self.P, self.Q, self.user_bias, self.item_bias]
+
+    def _update_prediction(
+        self,
+    ):
+        pred = torch.mm(self.P, self.Q) + self.bias + self.user_bias + self.item_bias.view(1, -1)
+
+        self.pred = pd.DataFrame(
+            pred.detach().numpy(),
+            index=self.users,
+            columns=self.items,
+        )
+
+    def fit(
+        self,
+        optimizer: Callable,
+        criterion: Callable,
+        n_epochs: int,
+        lr: float = 0.01,
+        beta: float = 0.01,
+    ) -> MFType:
+        self.optimizer = optimizer(self.trainable_params, lr=lr)
+        self.criterion = criterion()
+
+        y_true = torch.from_numpy(self.df.to_numpy()).float()
+        mask = ~torch.isnan(y_true)
+        pos_mask = mask.nonzero(as_tuple=True)
+        pos_user_mask, pos_item_mask = pos_mask[0], pos_mask[1]
+
+        for e in range(n_epochs):
+            y_pred = (
+                torch.mm(self.P, self.Q)[mask]
+                + self.bias
+                + self.user_bias[pos_user_mask].view(-1)
+                + self.item_bias[pos_item_mask].view(-1)
+            )
+
+            penalty = beta * (self.P.pow(2).sum() + self.Q.pow(2).sum())
+            loss = self.criterion(y_true[mask], y_pred) + penalty
+
+            loss.backward()
+            self.optimizer.step()
+            self.optimizer.zero_grad()
+
+            self.loss.append(loss.item())
+
+        self._update_prediction()
+
+        return self
+
+    def predict(
+        self,
+        user: Union[str, int, float],
+        item: Union[str, int, float],
+    ) -> float:
+        if user not in self.users:
+            return 3.0
+
+        if item not in self.items:
+            return 3.0
+
+        return self.pred.loc[user, item]
